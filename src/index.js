f37f2a0f7a0d933b8882c9b4ca02e8ab9381e113
alexmost <alexmost1989@gmail.com>
2025-03-11 13:53:36 +0100

    Basic implementation for the plugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,69 @@
-module.exports = function () {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
-      Identifier(path) {
-        // Example transformation: rename all identifiers 'foo' to 'bar'
-        if (path.node.name === 'foo') {
-          path.node.name = 'bar';
-        }
+      Program(path) {
+        const extractedArraysAndObjects = [];
+
+        // Function to check if all elements in an array are primitives
+        const areAllPrimitives = (node) => {
+          if (t.isArrayExpression(node)) {
+            return node.elements.every((el) => areAllPrimitives(el));
+          }
+          if (t.isObjectExpression(node)) {
+            return node.properties.every(
+              (prop) => t.isObjectProperty(prop) && areAllPrimitives(prop.value)
+            );
+          }
+          return t.isLiteral(node); // Check if it's a primitive (including strings, numbers, booleans)
+        };
+
+        path.traverse({
+          JSXAttribute(innerPath) {
+            if (!t.isJSXExpressionContainer(innerPath.node.value)) return;
+
+            const expr = innerPath.node.value.expression;
+
+            // Check if the expression is an object or array
+            if (!t.isObjectExpression(expr) && !t.isArrayExpression(expr))
+              return;
+
+            // Ensure all properties or elements are primitives
+            if (!areAllPrimitives(expr)) return;
+
+            const componentParent = innerPath.findParent(
+              (path) =>
+                path.isFunctionDeclaration() ||
+                path.isArrowFunctionExpression() ||
+                path.isClassDeclaration()
+            );
+
+            if (!componentParent) return;
+
+            const componentName = componentParent.node.id
+              ? componentParent.node.id.name
+              : 'Anonymous';
+
+            const uniqueVarName = innerPath.scope.generateUidIdentifier(
+              `${componentName}_${innerPath.node.name.name}`
+            );
+            extractedArraysAndObjects.push({
+              identifier: uniqueVarName,
+              expression: expr,
+            });
+
+            // Replace the original object or array with the new identifier
+            innerPath.node.value = t.jSXExpressionContainer(uniqueVarName);
+          },
+        });
+
+        // Insert all new variable declarations at the top of the module
+        extractedArraysAndObjects.forEach(({ identifier, expression }) => {
+          path.node.body.unshift(
+            t.variableDeclaration('const', [
+              t.variableDeclarator(identifier, expression),
+            ])
+          );
+        });
       },
     },
   };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,26 +1,211 @@
 const babel = require('@babel/core');
 const plugin = require('../src/index');
 
-describe('Babel Plugin', () => {
-  it('should rename identifier "foo" to "bar"', () => {
-    const inputCode = `const foo = 'Hello, world!';\nconsole.log(foo);`;
-    const outputCode = `const bar = 'Hello, world!';\nconsole.log(bar);`;
+const normalizeWhitespace = (code) => code.replace(/\s+/g, ' ').trim();
 
-    const { code } = babel.transformSync(inputCode, {
+const transform = (code) => {
+  return normalizeWhitespace(
+    babel.transformSync(code, {
       plugins: [plugin],
-    });
+      presets: ['@babel/preset-react'],
+    }).code
+  );
+};
 
-    expect(code).toBe(outputCode);
+describe('babel-plugin-jsx-attribute-to-const', () => {
+  test('transforms multiple attributes with array and object to constants', () => {
+    const inputCode = `
+      const MyComponent = () => {
+        return (
+          <Box
+            box-padding={['m', 'l@large']}
+            style={{ width: 100 }}
+            customData={{ key: 'value' }}
+          />
+        );
+      };
+    `;
+
+    const expectedOutput = normalizeWhitespace(`
+      const _Anonymous_customData = { key: 'value' };
+      const _Anonymous_style = { width: 100 };
+      const _Anonymous_boxPadding = ['m', 'l@large'];
+      const MyComponent = () => {
+        return /*#__PURE__*/React.createElement(Box, {
+          "box-padding": _Anonymous_boxPadding,
+          style: _Anonymous_style,
+          customData: _Anonymous_customData
+        });
+      };
+    `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+});
+
+describe('babel-plugin-jsx-attribute-to-const objects', () => {
+  test('transforms simple object JSX attribute to constant', () => {
+    const inputCode = `
+      const MyComponent = () => {
+        return <Box style={{ width: 100, height: 200 }} />;
+      };
+    `;
+
+    const expectedOutput = normalizeWhitespace(`
+      const _Anonymous_style = { width: 100, height: 200 };
+      const MyComponent = () => {
+        return /*#__PURE__*/React.createElement(Box, {
+        style: _Anonymous_style
+      });
+    };
+    `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
   });
 
-  it('should not change other identifiers', () => {
-    const inputCode = `const baz = 'Hello, world!';\nconsole.log(baz);`;
-    const outputCode = `const baz = 'Hello, world!';\nconsole.log(baz);`;
+  test('transforms nested simple object JSX attribute to constant', () => {
+    const inputCode = `
+      const MyComponent = () => {
+        return <Box style={{ width: [100, 200], height: 200 }} />;
+      };
+    `;
 
-    const { code } = babel.transformSync(inputCode, {
-      plugins: [plugin],
-    });
+    const expectedOutput = normalizeWhitespace(`
+      const _Anonymous_style = { width: [100, 200], height: 200 };
+      const MyComponent = () => {
+        return /*#__PURE__*/React.createElement(Box, {
+        style: _Anonymous_style
+      });
+    };
+    `);
 
-    expect(code).toBe(outputCode);
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+
+  test('handles two components with overlapping attribute names', () => {
+    const inputCode = `
+      const FirstComponent = () => {
+        return <Box boxPadding={{ size: 'small' }} />;
+      };
+
+      const SecondComponent = () => {
+        return <Box boxPadding={{ size: 'large' }} />;
+      };
+    `;
+
+    const outputCode = transform(inputCode);
+    const expectedOutput = normalizeWhitespace(`
+    const _Anonymous_boxPadding2 = { size: 'large' };
+    const _Anonymous_boxPadding = { size: 'small' };
+    const FirstComponent = () => {
+      return /*#__PURE__*/React.createElement(Box, { boxPadding: _Anonymous_boxPadding });
+    };
+    const SecondComponent = () => {
+      return /*#__PURE__*/React.createElement(Box, { boxPadding: _Anonymous_boxPadding2 });
+    };
+    `);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+
+  test('should not transform to constant if object has non-primitive value', () => {
+    const inputCode = `
+    const MyComponent = (props) => {
+      return <Box box-padding={{ size: props.size, color: 'blue' }} />;
+    };
+  `;
+
+    const expectedOutput = normalizeWhitespace(`
+    const MyComponent = props => {
+      return /*#__PURE__*/React.createElement(Box, { "box-padding": { size: props.size, color: 'blue' } });
+    };
+  `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+});
+
+describe('babel-plugin-jsx-attribute-to-const arrays', () => {
+  test('transforms simple array JSX attribute to constant', () => {
+    const inputCode = `
+      const MyComponent = () => {
+        return <Box box-padding={['m', 'l@large']} />;
+      };
+    `;
+
+    const expectedOutput = normalizeWhitespace(`
+      const _Anonymous_boxPadding = ['m', 'l@large'];
+      const MyComponent = () => {
+        return /*#__PURE__*/React.createElement(Box, {
+        "box-padding": _Anonymous_boxPadding
+      });
+    };
+    `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+
+  test('should not transform to constant if array has no primitive value', () => {
+    const inputCode = `
+      const MyComponent = (props) => {
+        return <Box box-padding={[props.m, 'l@large']} />;
+      };
+    `;
+
+    const expectedOutput = normalizeWhitespace(`
+    const MyComponent = props => {
+      return /*#__PURE__*/React.createElement(Box, { "box-padding": [props.m, 'l@large'] });
+    };
+    `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+
+  test('handles two components with overlapping attribute names', () => {
+    const inputCode = `
+      const FirstComponent = () => {
+        return <Box boxPadding={['m', 'small' ]} />;
+      };
+
+      const SecondComponent = () => {
+        return <Box boxPadding={['m','large' ]} />;
+      };
+    `;
+
+    const outputCode = transform(inputCode);
+    const expectedOutput = normalizeWhitespace(`
+    const _Anonymous_boxPadding2 = ['m', 'large'];
+    const _Anonymous_boxPadding = ['m', 'small'];
+    const FirstComponent = () => {
+      return /*#__PURE__*/React.createElement(Box, { boxPadding: _Anonymous_boxPadding });
+    };
+    const SecondComponent = () => {
+      return /*#__PURE__*/React.createElement(Box, { boxPadding: _Anonymous_boxPadding2 });
+    };
+    `);
+    expect(outputCode).toEqual(expectedOutput);
+  });
+
+  test('transforms nested simple arrays to constant', () => {
+    const inputCode = `
+      const MyComponent = () => {
+        return <Box style={[{ width: [100, 200]},  {height: 200 }]} />;
+      };
+    `;
+
+    const expectedOutput = normalizeWhitespace(`
+    const _Anonymous_style = [{ width: [100, 200] }, { height: 200 }];
+    const MyComponent = () => {
+      return /*#__PURE__*/React.createElement(Box, { style: _Anonymous_style });
+    };
+    `);
+
+    const outputCode = transform(inputCode);
+    expect(outputCode).toEqual(expectedOutput);
   });
 });
